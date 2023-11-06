57300199c1854c578c3fa469245a85a4eae712eb
Zidane Ibrahim Fadela <zidaneibrhmfdl7@gmail.com>
2023-11-06 10:56:39 +0700

    update sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 import SidebarDashboard from "./components/sidebar/SidebarDashboard";
 import SidebarLearning from "./components/sidebar/SidebarLearning";
 
 function App() {
   return (
     <div>
-      <SidebarDashboard />
+      {/* <SidebarDashboard /> */}
       <SidebarLearning />
     </div>
   );
--- src/components/sidebar/SidebarDashboard.jsx
+++ src/components/sidebar/SidebarDashboard.jsx
@@ -10,48 +10,52 @@ import { GiProgression } from 'react-icons/gi'
 
 function SidebarDashboard() {
   return (
-    <div>
-      <Box w='100%' h='100%' m='5px' p='3px' fontSize={'23px'}>
-        <Image
-          src='/src/assets/Logo Full.jpg'
-          alt='Logo Talenesia'
-          boxSize='13%'
-          objectFit='cover'
-        />
-        <UnorderedList m='10px' pt={1} fontFamily='overlock' fontSize={15}>
-          <ListItem listStyleType='none'>
-            <Flex align='center' >
-              <Box as={AiFillHome} mr={2} /><a href="#">Dashboard</a>
-            </Flex>
-          </ListItem>
-          <ListItem listStyleType='none' pt={5}>
-            <Flex align='center' >
-              <Box as={BsBookFill} mr={2} /><a href="#">KursusKu</a>
-            </Flex>
-          </ListItem>
-          <ListItem listStyleType='none' pt={5}>
-            <Flex align='center' >
-              <Box as={CgProfile} mr={2} /><a href="#">Profile</a>
-            </Flex>
-          </ListItem>
-          <ListItem listStyleType='none' pt={5} position='relative'>
-            <Flex align='center' >
-              <Box as={GiProgression} mr={2} /><a href="#">Progress Belajar</a>
-            </Flex>
-          </ListItem>
-          <ListItem listStyleType='none' pt={5}>
-            <Flex align='center' >
-              <Box as={AiFillSetting} mr={2} /><a href="#">Setting</a>
-            </Flex>
-          </ListItem>
-          <ListItem listStyleType='none' pt={5}>
-            <Flex align='center' >
-              <Box as={BiLogOutCircle} mr={2} /><a href="#">Log Out</a>
-            </Flex>
-          </ListItem>
-        </UnorderedList>
+    <>
+      <Box width='20vw' boxShadow='dark-lg' height='100vh'>
+        <Box>
+          <Image
+            src='/src/assets/Logo Full.jpg'
+            alt='Logo Talenesia'
+            boxSize='100%'
+            objectFit='cover'
+          />
+        </Box>
+        <Box >
+          <UnorderedList m='10px' pt={1} fontFamily='overlock' fontSize={15} >
+            <ListItem listStyleType='none' _hover={{ background: "steelBlue", color: 'white' }} borderRadius='xl' p='4px' mt='4px' >
+              <Flex align='center' >
+                <Box as={AiFillHome} mr={2} /><a href="#">Dashboard</a>
+              </Flex>
+            </ListItem>
+            <ListItem listStyleType='none' pt={5} _hover={{ background: "steelBlue", color: 'white' }} borderRadius='xl' p='4px' mt='4px' >
+              <Flex align='center' >
+                <Box as={BsBookFill} mr={2} /><a href="#">KursusKu</a>
+              </Flex>
+            </ListItem>
+            <ListItem listStyleType='none' pt={5} _hover={{ background: "steelBlue", color: 'white' }} borderRadius='xl' p='4px' mt='4px'>
+              <Flex align='center' >
+                <Box as={CgProfile} mr={2} /><a href="#">Profile</a>
+              </Flex>
+            </ListItem>
+            <ListItem listStyleType='none' pt={5} position='relative' _hover={{ background: "steelBlue", color: 'white' }} borderRadius='xl' p='4px' mt='4px' >
+              <Flex align='center' >
+                <Box as={GiProgression} mr={2} /><a href="#">Progress Belajar</a>
+              </Flex>
+            </ListItem>
+            <ListItem listStyleType='none' pt={5} _hover={{ background: "steelBlue", color: 'white' }} borderRadius='xl' p='4px' mt='4px'>
+              <Flex align='center' >
+                <Box as={AiFillSetting} mr={2} /><a href="#">Setting</a>
+              </Flex>
+            </ListItem>
+            <ListItem listStyleType='none' pt={5} _hover={{ background: "steelBlue", color: 'white' }} borderRadius='xl' p='4px' mt='4px'>
+              <Flex align='center' >
+                <Box as={BiLogOutCircle} mr={2} /><a href="#">Log Out</a>
+              </Flex>
+            </ListItem>
+          </UnorderedList>
+        </Box>
       </Box>
-    </div >
+    </ >
   )
 }
 
