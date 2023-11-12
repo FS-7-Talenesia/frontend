056700abace57c3acc6e0bcd00273a9abcc8dcec
Rafi Alghifari <raffialg11@gmail.com>
2023-11-12 18:18:50 +0700

    feat: call Sidebar Component in Learning Screen Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // eslint-disable-next-line no-unused-vars
-// import Discussion from "./pages/discussion/discussion";
-// import Quiz from "./pages/quiz/quiz";
+import Discussion from "./pages/discussion/discussion";
+import Quiz from "./pages/quiz/quiz";
 import SidebarDashboard from "./components/sidebar/SidebarDashboard";
 import SidebarLearning from "./components/sidebar/SidebarLearning";
 
 function App() {
   return (
     <div>
-      {/* <Quiz /> */}
+      <Quiz />
       {/* <Discussion /> */}
       {/* <SidebarDashboard /> */}
-      <SidebarLearning />
+      {/* <SidebarLearning /> */}
     </div>
   );
 }
