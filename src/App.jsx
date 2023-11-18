74c5e4416eaf9f9b86becafb6dc173e99229d434
Zidane Ibrahim Fadela <zidaneibrhmfdl7@gmail.com>
2023-11-18 23:52:40 +0700

    add profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 import SidebarDashboard from "./components/sidebar/SidebarDashboard"
 import Dashboard from "./pages/dashboard/dashboard"
-import { Flex } from "@chakra-ui/react"
 import SidebarLearning from "./components/sidebar/SidebarLearning"
+import Profile from "./pages/profile/Profile"
+
+import { Flex } from "@chakra-ui/react"
 
 function App() {
   return (
     <div>
       <Flex>
         <SidebarDashboard />
-        <Dashboard />
+        <Profile />
       </Flex>
+      {/* <Dashboard /> */}
       {/* <SidebarLearning /> */}
     </div>
   )
