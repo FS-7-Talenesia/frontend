ea30c349c7a451d70c50eda715ab538ea685c582
Rafi Alghifari <raffialg11@gmail.com>
2023-11-20 15:15:09 +0700

    fix Merge Conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-// eslint-disable-next-line no-unused-vars
-import SidebarDashboard from "./components/sidebar/SidebarDashboard"
-import Dashboard from "./pages/dashboard/dashboard"
-import SidebarLearning from "./components/sidebar/SidebarLearning"
-import Profile from "./pages/profile/Profile"
-import Badges from "./components/progressBelajar/Badges"
-import Grade from "./components/progressBelajar/Grade"
+import SidebarDashboard from "./components/sidebar/SidebarDashboard";
+import Dashboard from "./pages/dashboard/dashboard";
+import SidebarLearning from "./components/sidebar/SidebarLearning";
+import Profile from "./pages/profile/Profile";
+import Badges from "./components/progressBelajar/Badges";
+import Grade from "./components/progressBelajar/Grade";
 
-import { Flex } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react";
+import Discussion from "./pages/discussion/discussion";
+import Quiz from "./pages/quiz/quiz";
+import Material from "./pages/material/material";
+import ForumDetail from "./pages/forumDetail/forumDetail";
+import PreQuiz from "./pages/preQuiz/preQuiz";
+import QuizReview from "./pages/quizreview/quizReview";
+import FileSubmission from "./pages/fileSubmission/fileSubmission";
 
 function App() {
   return (
@@ -19,8 +25,15 @@ function App() {
       </Flex>
       {/* <Dashboard /> */}
       {/* <SidebarLearning /> */}
+      {/* <Quiz /> */}
+      {/* <Discussion /> */}
+      {/* <Material /> */}
+      {/* <ForumDetail /> */}
+      {/* <PreQuiz /> */}
+      {/* <QuizReview /> */}
+      {/* <FileSubmission /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
